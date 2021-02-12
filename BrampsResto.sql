5990d816fda26b80739f4588444c4324c85afa45
JTanBaguisi09 <57487344+JTanBaguisi09@users.noreply.github.com>
2021-02-12 16:11:57 +0800

    Add files via upload
    First upload of the .bak File and SQL Query. Tables are not Populated yet with data
    Will update later by adding data to each table and adding necessary stored procedures needed by front end program

---
+++ BrampsResto.sql
@@ -0,0 +1,249 @@
+USE [BrampsResto]
+GO
+CREATE SCHEMA Bramps
+	CREATE TABLE Bramps.Customer
+	(
+		CustomerID INT IDENTITY (1,1) NOT NULL PRIMARY KEY,
+		UserName VARCHAR(200) NOT NULL UNIQUE,
+		Password VARCHAR(200) NOT NULL,
+		CustomerName VARCHAR(200) NOT NULL UNIQUE,
+		ContactNumber NVARCHAR(25) NOT NULL,
+		Address VARCHAR(200) NOT NULL,
+		DateCreated DATETIME2(3) DEFAULT (SYSDATETIME()) NOT NULL
+	)
+	CREATE TABLE Bramps.MenuType
+	(
+		MenuTypeID INT IDENTITY (1,1) NOT NULL PRIMARY KEY,
+		MenuType VARCHAR(100) NOT NULL UNIQUE,
+		TypeDescription VARCHAR(200) NOT NULL,
+		DateCreated DATETIME2(3) DEFAULT (SYSDATETIME()) NOT NULL
+	)
+	CREATE TABLE Bramps.MenuItem
+	(
+		MenuItemID INT IDENTITY (1,1) NOT NULL PRIMARY KEY,
+		MenuTypeID INT NOT NULL FOREIGN KEY REFERENCES MenuType (MenuTypeID),
+		MenuItemName VARCHAR(100) NOT NULL UNIQUE,
+		MenuItemPrice DECIMAL (18, 0) NOT NULL,
+		MenuItemDescription VARCHAR(200) NOT NULL,
+		DateCreated DATETIME2(3) DEFAULT (SYSDATETIME()) NOT NULL
+	)
+	CREATE TABLE Bramps.OrderMenu
+	(
+		OrderID INT IDENTITY (1,1) NOT NULL PRIMARY KEY,
+		CustomerID INT NOT NULL FOREIGN KEY REFERENCES Customer (CustomerID),
+		OrderList VARCHAR(MAX) NOT NULL,
+		OtherInstructions VARCHAR(MAX) NOT NULL,
+	    DateCreated DATETIME2(3) DEFAULT (SYSDATETIME()) NOT NULL
+	)
+
+--===================================================================================
+--===================================================================================
+--===================================================================================
+
+--================= MENU ITEM TABLE PROCEDURES ======================================
+USE [BrampsResto]
+GO
+SET ANSI_NULLS ON
+	GO
+	SET QUOTED_IDENTIFIER ON
+	GO
+	CREATE PROCEDURE [dbo].[AddNewMenuItem]
+		@IDItem INT,
+		@IDType INT,
+		@NameItem VARCHAR(50),
+		@DescItem Varchar (200),
+		@Price Decimal
+	AS
+	BEGIN
+		SET NOCOUNT ON;
+			INSERT INTO Bramps.MenuItem(MenuItemID, MenuTypeID, MenuItemName, MenuItemDescription, MenuItemPrice)
+			VALUES (@IDItem, @IDType, @NameItem, @DescItem, @Price)
+	END
+--=========================================================================================
+SET ANSI_NULLS ON
+	GO
+	SET QUOTED_IDENTIFIER ON
+	GO
+	CREATE PROCEDURE [dbo].[DeleteMenuItem]
+		@IDItem INT
+	AS
+	BEGIN
+		DELETE FROM Bramps.MenuItem WHERE MenuItemID = @IDItem
+	END
+--=========================================================================================
+SET ANSI_NULLS ON
+	GO
+	SET QUOTED_IDENTIFIER ON
+	GO
+	CREATE PROCEDURE [dbo].[GetMenuItem]
+		@IDItem INT
+	AS
+	BEGIN
+		SELECT MenuItemID, MenuTypeID, MenuItemName, MenuItemPrice, MenuItemDescription
+		FROM Bramps.MenuItem  WHERE MenuItemID = @IDItem
+	END
+--=========================================================================================
+SET ANSI_NULLS ON
+	GO
+	SET QUOTED_IDENTIFIER ON
+	GO
+	CREATE PROCEDURE [dbo].[GetMenuItemList]
+	AS
+	BEGIN
+		SELECT MenuItemID, MenuTypeID, MenuItemName, MenuItemPrice, MenuItemDescription
+		FROM Bramps.MenuItem
+	END
+--=========================================================================================
+SET ANSI_NULLS ON
+	GO
+	SET QUOTED_IDENTIFIER ON
+	GO
+	CREATE PROCEDURE [dbo].[SearchMenuItem]
+		@NameMenuItem VARCHAR(100)
+	AS
+	BEGIN
+		SET NOCOUNT ON;
+		SELECT MenuItemID, MenuTypeID, MenuItemName, MenuItemPrice, MenuItemDescription
+		FROM Bramps.MenuItem
+		WHERE MenuItemName LIKE @NameMenuItem
+	END
+--=========================================================================================
+--=========================================================================================
+--=========================================================================================
+
+
+--================================ CUSTOMER TABLE PROCEDURES ==============================
+SET ANSI_NULLS ON
+	GO
+	SET QUOTED_IDENTIFIER ON
+	GO
+	CREATE PROCEDURE [dbo].[AddNewCustomer]
+		@IDCustomer INT,
+		@Uname VARCHAR(100),
+		@PWord VARCHAR(100),
+		@NameCustomer VARCHAR (200),
+		@Contact NVARCHAR (50),
+		@Location VARCHAR (200)
+	AS
+	BEGIN
+		SET NOCOUNT ON;
+			INSERT INTO Bramps.Customer(CustomerID, Username, Password, CustomerName, ContactNumber, Address)
+			VALUES (@IDCustomer, @Uname, @PWord, @NameCustomer, @Contact, @Location)
+	END
+--=========================================================================================
+SET ANSI_NULLS ON
+	GO
+	SET QUOTED_IDENTIFIER ON
+	GO
+	CREATE PROCEDURE [dbo].[DeleteCustomer]
+		@IDCustomer INT
+	AS
+	BEGIN
+		DELETE FROM Bramps.Customer WHERE CustomerID = @IDCustomer
+	END
+--=========================================================================================
+SET ANSI_NULLS ON
+	GO
+	SET QUOTED_IDENTIFIER ON
+	GO
+	CREATE PROCEDURE [dbo].[GetCustomer]
+		@IDCustomer INT
+	AS
+	BEGIN
+		SELECT CustomerID, Username, Password, CustomerName, ContactNumber, Address
+		FROM Bramps.Customer  WHERE CustomerID = @IDCustomer
+	END
+--=========================================================================================
+SET ANSI_NULLS ON
+	GO
+	SET QUOTED_IDENTIFIER ON
+	GO
+	CREATE PROCEDURE [dbo].[GetCustomerList]
+	AS
+	BEGIN
+		SELECT CustomerID, Username, Password, CustomerName, ContactNumber, Address
+		FROM Bramps.Customer
+	END
+--=========================================================================================
+SET ANSI_NULLS ON
+	GO
+	SET QUOTED_IDENTIFIER ON
+	GO
+	CREATE PROCEDURE [dbo].[SearchCustomer]
+		@NameCustomer VARCHAR(100)
+	AS
+	BEGIN
+		SET NOCOUNT ON;
+		SELECT CustomerID, Username, Password, CustomerName, ContactNumber, Address
+		FROM Bramps.Customer
+		WHERE CustomerName LIKE @NameCustomer
+	END
+--=========================================================================================
+--=========================================================================================
+--=========================================================================================
+
+--============================= ORDER MENU TABLE PROCEDURES ===============================
+SET ANSI_NULLS ON
+	GO
+	SET QUOTED_IDENTIFIER ON
+	GO
+	CREATE PROCEDURE [dbo].[AddNewOrder]
+		@IDOrder INT,
+		@IDCustomer INT,
+		@ListOrder VARCHAR (MAX),
+		@Instructions Varchar (MAX),
+		@Price Decimal
+	AS
+	BEGIN
+		SET NOCOUNT ON;
+			INSERT INTO Bramps.OrderMenu(OrderID, CustomerID, OrderList, OtherInstructions)
+			VALUES (@IDOrder, @IDCustomer, @ListOrder, @Instructions)
+	END
+--=========================================================================================
+SET ANSI_NULLS ON
+	GO
+	SET QUOTED_IDENTIFIER ON
+	GO
+	CREATE PROCEDURE [dbo].[DeleteOrder]
+		@IDOrder INT
+	AS
+	BEGIN
+		DELETE FROM Bramps.OrderMenu WHERE OrderID = @IDOrder
+	END
+--=========================================================================================
+SET ANSI_NULLS ON
+	GO
+	SET QUOTED_IDENTIFIER ON
+	GO
+	CREATE PROCEDURE [dbo].[GetOrder]
+		@IDOrder INT
+	AS
+	BEGIN
+		SELECT OrderID, CustomerID, OrderList, OtherInstructions
+		FROM Bramps.OrderMenu  WHERE OrderID = @IDOrder
+	END
+--=========================================================================================
+SET ANSI_NULLS ON
+	GO
+	SET QUOTED_IDENTIFIER ON
+	GO
+	CREATE PROCEDURE [dbo].[GetOrderList]
+	AS
+	BEGIN
+		SELECT OrderID, CustomerID, OrderList, OtherInstructions
+		FROM Bramps.OrderMenu
+	END
+--=========================================================================================
+SET ANSI_NULLS ON
+	GO
+	SET QUOTED_IDENTIFIER ON
+	GO
+	CREATE PROCEDURE [dbo].[SearchOrderByCustomer]
+		@IDCustomer VARCHAR(100)
+	AS
+	BEGIN
+		SET NOCOUNT ON;
+		SELECT OrderID, CustomerID, OrderList, OtherInstructions
+		FROM Bramps.OrderMenu
+		WHERE CustomerID LIKE @IDCustomer
+	END
