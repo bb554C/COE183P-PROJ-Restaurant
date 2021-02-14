b0cd6f9b336012ecfe22d17c01bb6a9b9978fdaa
Baird Rouan S. Buenaventura <bairdbuenaventura@gmail.com>
2021-02-14 21:26:25 +0800

    GUI V2

--- BrampsRestaurantOnline/BrampsRestaurantOnline/Home.aspx.cs
+++ BrampsRestaurantOnline/BrampsRestaurantOnline/Home.aspx.cs
@@ -1,14 +1,12 @@
 ﻿using System;
-using System.Linq;
-using System.Web.ModelBinding;
+
 namespace BrampsRestaurantOnline
 {
-    public partial class FoodDescription : System.Web.UI.Page
+    public partial class Home : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
-
     }
 }
