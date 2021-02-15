e43caed002c59d3ea243289b1d966ec3d80cc293
Alexcaabay <alexcaabay2@gmail.com>
2021-02-15 18:07:13 +0800

    Partial class for food and customer

---
+++ BrampsRestaurantOnline/BrampsRestaurantOnline/Classes/FoodClass.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace BrampsRestaurantOnline.Classes
+{
+    public class FoodClass
+    {
+        public static void AddNewMenuItem
+        (int MenuItemID, int MenuTypeID, string MenuItemName, string MenuItemDescription, decimal Price)
+        {
+            SqlConnection SQLConn = SQLConnection.SQLConnect();
+            try
+            {
+                using (SqlCommand command = new SqlCommand("dbo.AddNewMenuItem", SQLConn))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.Add("@IDItem", SqlDbType.Int).Value = MenuItemID;
+                    command.Parameters.Add("@IDType", SqlDbType.Int).Value = MenuTypeID;
+                    command.Parameters.Add("@NameItem", SqlDbType.VarChar, 100).Value = MenuItemName;
+                    command.Parameters.Add("@DescItem", SqlDbType.VarChar, 100).Value = MenuItemDescription;
+                    command.Parameters.Add("@Price", SqlDbType.Decimal).Value = Price;
+                    SQLConn.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            SQLConnection.CloseSQLConnect(SQLConn);
+
+        }
+        public static void AddNewOrder
+         (int OrderID, int CustomerID, string OrderList, string Instructions, decimal Price)
+        {
+            SqlConnection SQLConn = SQLConnection.SQLConnect();
+            try
+            {
+                using (SqlCommand command = new SqlCommand("dbo.AddNewOrder", SQLConn))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.Add("@IDOrder", SqlDbType.Int).Value = OrderID;
+                    command.Parameters.Add("@IDCustomer", SqlDbType.Int).Value = CustomerID;
+                    command.Parameters.Add("@ListOrder", SqlDbType.VarChar, 100).Value = OrderList;
+                    command.Parameters.Add("@Instructions", SqlDbType.VarChar, 100).Value = Instructions;
+                    SQLConn.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+             
+            }
+            SQLConnection.CloseSQLConnect(SQLConn); 
+            
+        }
+        public static void DeleteOrder(int OrderID)
+        {
+            SqlConnection SQLconn = SQLConnection.SQLConnect();
+            try
+            {
+                using (SqlCommand command = new SqlCommand("dbo.DeleteOrder", SQLconn))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.Add("@IDOrder", SqlDbType.Int).Value = OrderID;
+                    SQLconn.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+              
+            }
+            SQLConnection.CloseSQLConnect(SQLconn);
+        }
+    }
+}
